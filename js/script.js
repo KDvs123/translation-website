bb6cd8d129ef7182132cec052e9d34b52f3da0ca
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-12 21:59:29 +0530

    implemented the code so that it will select the English as From language and Sinhala as to language

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const selectTag=document.querySelectorAll("select")
 
-selectTag.forEach(tag=>{
+selectTag.forEach((tag,id)=>{
     for(const country_code in countries){
-        console.log(countries[country_code])
-        let option=`  <option value="${country_code}">${countries[country_code]}</option>`;
+        //selecting English by default as From language and Sinhala as to Language
+        let selected;
+        if(id==0 && country_code== "en-GB"){
+            selected="selected";
+        }
+        if (id == 1 && country_code == "si-LK") {
+          selected = "selected";
+        }
+
+        let option=`  <option value="${country_code}" ${selected}>${countries[country_code]}</option>`;
         tag.insertAdjacentHTML("beforeend",option) //adding options tag inside select tag
 
          
